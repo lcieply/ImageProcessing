e21461dc3a90d0717d0c711aea6892001ea6bb71
Kamil Jureczka <kamil.jureczka@hotmail.com>
2017-05-15 19:11:01 +0200

    Initial commit

---
+++ src/main/java/pl/edu/agh/Main.java
@@ -0,0 +1,10 @@
+package pl.edu.agh;
+
+/**
+ * Created by Kamil Jureczka on 2017-05-15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Projekt1");
+    }
+}
